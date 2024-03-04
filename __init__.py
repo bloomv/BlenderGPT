c44cb8d95833a009e946ea9e655dab1c255bfc27
深圳弘谷科技有限公司 <genwise@gmail.com>
2024-03-04 14:22:18 +0800

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -59,25 +59,7 @@ system_prompt = """You are an assistant made for the purposes of helping the use
 - Do not perform destructive operations on the meshes. 
 - Do not use cap_ends. 
 - Do not respond with anything that is not Python code.
-
-Example:
-
-user: create 10 cubes in random locations from -10 to 10
-assistant:
-```
-import bpy
-import random
-bpy.ops.mesh.primitive_cube_add()
-
-#how many cubes you want to add
-count = 10
-
-for c in range(0,count):
-    x = random.randint(-10,10)
-    y = random.randint(-10,10)
-    z = random.randint(-10,10)
-    bpy.ops.mesh.primitive_cube_add(location=(x,y,z))
-```"""
+"""
 
 
 class GPT4_OT_DeleteMessage(bpy.types.Operator):
